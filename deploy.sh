51e12af5e7015c0f5cf5a04e67fdc632e155a057
Sam Warnick <sam@warnick.me>
2025-12-31 14:57:34 -0500

    Add coolify prep playbook

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,6 @@ terraform apply -auto-approve
 # Configure VMs
 cd ../ansible
 ansible-playbook -i inventory.yml playbooks/ubuntu-setup.yml
+ansible-playbook -i inventory.yml playbooks/coolify-setup.yml
 
 echo "Deployment complete!"
